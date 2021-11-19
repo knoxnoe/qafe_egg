a4ed63ef3b9e422f92f38ca3c90e8e06ce167524
goutongwang <goutongwang@bytedance.com>
2021-11-19 20:46:44 +0800

    fix: fix

--- app/model/question.ts
+++ app/model/question.ts
@@ -2,7 +2,10 @@ import { QuestionSchema } from './schema/index';
 
 export default app => {
 
-  const Question = app.model.define('question', QuestionSchema(app.Sequelize))
+  const Question = app.model.define('question', QuestionSchema(app.Sequelize), {
+    timestamps: false, // 去掉模型默认的createAt updateAt
+    freezeTableName: true, // 使用自定义表名
+  })
 
   return Question;
 }
--- app/model/record.ts
+++ app/model/record.ts
@@ -1,7 +1,10 @@
 import { RecordSchema } from './schema/index';
 
 export default app => {
-  const Record = app.model.define('record', RecordSchema(app.Sequelize))
+  const Record = app.model.define('record', RecordSchema(app.Sequelize), {
+    timestamps: false, // 去掉模型默认的createAt updateAt
+    freezeTableName: true, // 使用自定义表名
+  })
   
   return Record;
 }
